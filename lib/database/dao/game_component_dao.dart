10efc9d0db4c9a7332b8f2401fbe418ef5b01c8f
Guilherme Garcia <gui123.garcia@gmail.com>
2024-03-07 19:26:41 -0300

    Added all game_component images

--- lib/database/dao/game_component_dao.dart
+++ lib/database/dao/game_component_dao.dart
@@ -1,4 +1,5 @@
 import 'package:fono_terapia/shared/model/game_component.dart';
+import 'package:fono_terapia/shared/utils/data.dart';
 import 'package:sqflite/sqflite.dart';
 
 class GameComponentDao {
@@ -8,41 +9,6 @@ class GameComponentDao {
   static const _imagePath = 'image_path';
   static const _audioPath = 'audio_path';
   static const _section = 'section';
-
-  static const nameList = [
-    'Café',
-    'Pão',
-    'Leite',
-    'Carro',
-    'Pato',
-    'Gato',
-    'O homem está comendo',
-    'O homem está dormindo',
-  ];
-
-  static const imagePathList = [
-    'assets/db_images/cafe.jpg',
-    'assets/db_images/pao.jpg',
-    'assets/db_images/leite.jpg',
-    'assets/db_images/carro.jpg',
-    'assets/db_images/pato.jpg',
-    'assets/db_images/gato.jpg',
-    'assets/db_images/o_homem_esta_comendo.jpg',
-    'assets/db_images/o_homem_esta_dormindo.jpg',
-  ];
-
-  static const audioPathList = [
-    'db_audios/cafe.mp3',
-    'db_audios/pao.mp3',
-    'db_audios/leite.mp3',
-    'db_audios/carro.mp3',
-    'db_audios/pato.mp3',
-    'db_audios/gato.mp3',
-    'db_audios/o_homem_esta_comendo.mp3',
-    'db_audios/o_homem_esta_dormindo.mp3',
-  ];
-
-  static const sectionList = [1, 1, 1, 1, 2, 2, 3, 3];
 
   static String get tableSql {
     return 'CREATE TABLE $_tableName('
@@ -54,15 +20,22 @@ class GameComponentDao {
   }
 
   static String get tableData {
-    return "INSERT INTO $_tableName ($_name, $_imagePath, $_audioPath, $_section) VALUES "
-        "('${nameList[0]}', '${imagePathList[0]}', '${audioPathList[0]}', ${sectionList[0]}), "
-        "('${nameList[1]}', '${imagePathList[1]}', '${audioPathList[1]}', ${sectionList[1]}), "
-        "('${nameList[2]}', '${imagePathList[2]}', '${audioPathList[2]}', ${sectionList[2]}), "
-        "('${nameList[3]}', '${imagePathList[3]}', '${audioPathList[3]}', ${sectionList[3]}), "
-        "('${nameList[4]}', '${imagePathList[4]}', '${audioPathList[4]}', ${sectionList[4]}), "
-        "('${nameList[5]}', '${imagePathList[5]}', '${audioPathList[5]}', ${sectionList[5]}), "
-        "('${nameList[6]}', '${imagePathList[6]}', '${audioPathList[6]}', ${sectionList[6]}), "
-        "('${nameList[7]}', '${imagePathList[7]}', '${audioPathList[7]}', ${sectionList[7]}); ";
+    if (nameList.length != imagePathList.length ||
+        nameList.length != audioPathList.length ||
+        nameList.length != sectionList.length) {
+      throw Exception('Arrays têm tamanhos diferentes');
+    }
+    String insertQuery =
+        "INSERT INTO $_tableName ($_name, $_imagePath, $_audioPath, $_section) VALUES ";
+
+    for (int i = 0; i < nameList.length; i++) {
+      insertQuery +=
+          "('${nameList[i]}', '${imagePathList[i]}', '${audioPathList[i]}', ${sectionList[i]}), ";
+    }
+
+    // Remover a vírgula extra no final
+    insertQuery = insertQuery.substring(0, insertQuery.length - 2);
+    return insertQuery;
   }
 
   Future<List<GameComponent>> findRandomComponents(
@@ -88,7 +61,7 @@ class GameComponentDao {
   GameComponent getRightAnswer(List<GameComponent> gameComponents) {
     List<GameComponent> temporaryList = List.from(gameComponents);
     temporaryList.shuffle();
-    
+
     return temporaryList.first;
   }
 
