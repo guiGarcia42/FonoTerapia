9bd7e1c78425ce8b7a4d61ac85b4d0139d44e232
Guilherme Garcia <gui123.garcia@gmail.com>
2024-03-19 14:50:30 -0300

    removed unused imports

--- lib/modules/game/widgets/build_game_list_of_options.dart
+++ lib/modules/game/widgets/build_game_list_of_options.dart
@@ -1,7 +1,6 @@
 import 'package:diacritic/diacritic.dart';
 import 'package:flutter/material.dart';
 import 'package:fono_terapia/app_startup.dart';
-import 'package:fono_terapia/database/dao/game_component_dao.dart';
 import 'package:fono_terapia/shared/assets/app_colors.dart';
 import 'package:fono_terapia/shared/assets/app_text_styles.dart';
 import 'package:fono_terapia/shared/model/game_component.dart';
